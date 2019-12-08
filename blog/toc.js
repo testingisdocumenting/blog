0214860b0a27da7bec1a28b8e00249dc4d9bbc26
Mykola <golubev.nikolay@gmail.com>
2019-12-07 22:19:19 -0500

    Updating blog

--- blog/toc.js
+++ blog/toc.js
@@ -15,20 +15,26 @@ toc = [ {
   "dirName" : "entry",
   "items" : [ {
     "sectionTitle" : "Entry",
-    "pageTitle" : "Testing Makes You Fast Day One",
+    "pageTitle" : "Testing Makes You Faster Day One",
     "pageMeta" : {
       "date" : [ "2019-11-25" ],
       "summary" : [ "How testing makes you fast day one and not in some unknown future" ]
     },
     "dirName" : "entry",
-    "fileName" : "testing-makes-you-fast-day-one",
-    "viewOnRelativePath" : "articles/testing-makes-you-fast-day-one.md",
+    "fileName" : "testing-makes-you-faster-day-one",
+    "viewOnRelativePath" : "articles/testing-makes-you-faster-day-one.md",
     "pageSectionIdTitles" : [ {
       "title" : "Tests Are Here To Speed You Up",
       "id" : "tests-are-here-to-speed-you-up"
     }, {
       "title" : "Building A Medieval Castle",
       "id" : "building-a-medieval-castle"
+    }, {
+      "title" : "Teleports System",
+      "id" : "teleports-system"
+    }, {
+      "title" : "Implementing Teleports",
+      "id" : "implementing-teleports"
     } ]
   } ]
 } ]
