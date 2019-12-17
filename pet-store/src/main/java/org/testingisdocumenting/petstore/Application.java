25e25c7d9f307a4aa404f8a144e5702598c4ed73
Mykola Golubyev <golubev.nikolay@gmail.com>
2019-12-16 23:28:00 -0500

    initial (#6)

---
+++ pet-store/src/main/java/org/testingisdocumenting/petstore/Application.java
@@ -0,0 +1,9 @@
+package org.testingisdocumenting.petstore;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
