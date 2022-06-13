a5146fb08c20aa97cf3c0515e022fb67469eb420
mykolagolubyev <golubev.nikolay@gmail.com>
2022-06-13 17:21:13 -0400

    update webtau and znai

--- game-store/game-store-tests/game-store-tests-e2e/src/test/groovy/basicScenarios/webtau.cfg.groovy
+++ game-store/game-store-tests/game-store-tests-e2e/src/test/groovy/basicScenarios/webtau.cfg.groovy
@@ -2,7 +2,7 @@ package basicScenarios
 
 url = "http://localhost:8080" // base url for all http and browser requests
 browserUrl = "http://localhost:3000" // base url for browser open commands. Overrides url above if both are present
-browserId = "firefox" // specifying browser to use for browser based operations
+browserId = "chrome" // specifying browser to use for browser based operations
 
 environments {
     dev { // optional overrides for the configs for dev environment
